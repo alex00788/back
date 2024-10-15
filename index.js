e7589b5c6383ee92ef3a92f03ee5bca32d881c9f
webbalenciaproject <web_balelcia_project@mail.ru>
2024-10-15 15:40:40 +0300

    websoket кнопка заблокировать разблокировать usera

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const cookieParser = require('cookie-parser')
 
 
-const app = express()
+const app = express()   // приложение
+//настройки приложения
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
@@ -52,3 +53,25 @@ const start = async () => {
 }
 
 start();
+
+
+//webSocketServer на порту 3500
+const WebSocket = require('ws');    //подключаем ws после установки пакетов    npm install ws
+const connectedUsers = []       //пользователи, которые подключились
+const PORT_WS = process.env.PORT_WS || 3700
+const wsServer = new WebSocket.WebSocketServer({ port: PORT_WS });
+wsServer.on('connection', (socket) => {
+    //socket.send(`connected`)                          //отправляю сообщение на фронт что сойдинился
+    connectedUsers.push(socket)                        // добавляю в массив подключенных онлайн
+    socket.on('message', (message) => {                  //когда получу сообщение
+        console.log(`67 !!!!!!Получено сообщение: ${message}`);
+        connectedUsers.forEach(s => {                        //иду по всем кто подключен
+            s.send(JSON.stringify(`${message}`))    // говорю что данные изменились
+        })
+    });
+    socket.on('close', () => {
+        console.log('73close!!!!')
+        socket.send(`Соединение закрыто`);
+    });
+});
+
